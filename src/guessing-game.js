38240d7ae4c19af00f45490ef0b855afdde1d4ca
Nick Goroshko <n.goroshko2000@gmail.com>
2019-10-12 19:55:36 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = Number.MAX_SAFE_INTEGER;
+        this.max = Number.MIN_SAFE_INTEGER;
+        this.last_guess = 0;
+    }
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
     }
 
     guess() {
-
+        var sum=this.max+this.min;
+        this.last_guess=Math.floor((sum+sum%2)/2);
+        return (this.last_guess);
     }
 
     lower() {
-
+        this.max=this.last_guess;
     }
 
     greater() {
-
+        this.min=this.last_guess;
     }
 }
 
